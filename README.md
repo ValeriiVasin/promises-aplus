0b86e2eff9bc4795fe6a08d9527637891a559d04
Valerii Vasin <valerii.vasin@gmail.com>
2020-11-19 00:57:01 +0100

    chore: initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Promise A+
+
+## Spec
+
+https://github.com/promises-aplus/promises-spec
+
+## Tests
+
+https://github.com/promises-aplus/promises-tests
---
+++ src/adapter.ts
@@ -0,0 +1,28 @@
+// suppress node warnings regarding the unhandled rejections
+// that are coming from the test
+function noop() {}
+process.on('unhandledRejection', noop);
+process.on('rejectionHandled', noop);
+
+export function resolved(value: any) {
+  return Promise.resolve(value);
+}
+
+export function rejected(reason: string) {
+  return Promise.reject(reason);
+}
+
+export function deferred() {
+  let resolve;
+  let reject;
+  const promise = new Promise((_resolve, _reject) => {
+    resolve = _resolve;
+    reject = _reject;
+  });
+
+  return {
+    promise,
+    reject,
+    resolve,
+  };
+}
